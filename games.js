412c4de54dc088596d42b949ce315aa43372f472
Nostalgia Arcade <nostalgiaarcade@outlook.es>
2026-07-14 11:15:35 +0200

    Update games.js

--- games.js
+++ games.js
@@ -1,6 +1,14 @@
 const games = [
 
 {
+    nombre:"Pang 3",
+    categoria:"Acción",
+    jugadores:"1-2",
+    imagen:"https://i.ibb.co/M6BqqjN/pang-3.jpg",
+    descripcion:"14/07/2026",
+    link:"shrinkme.click/x2UHp"
+},
+    {
     nombre:"Robocop 2",
     categoria:"Acción",
     jugadores:"1-2",
@@ -8,14 +16,5 @@ const games = [
     descripcion:"14/07/2026",
     link:"https://shrinkme.click/vgD8TR"
 },
-
-{
-    nombre:"Metal Slug",
-    categoria:"Acción",
-    jugadores:"1-2",
-    imagen:"https://....jpg",
-    descripcion:"Clásico arcade de SNK",
-    link:"https://...."
-}
 
 ];
