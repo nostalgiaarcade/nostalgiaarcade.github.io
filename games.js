f30782eb9438d351d798d999c238d14f9011695b
Nostalgia Arcade <nostalgiaarcade@outlook.es>
2026-07-14 04:16:01 +0200

    Update games.js

--- games.js
+++ games.js
@@ -1,1 +1,8 @@
-
+{
+nombre:"Robocop 2",
+categoria:"Acción",
+jugadores:"1-2",
+imagen:"https://i.ibb.co/zh8psvqz/Robocop-2.jpg",
+descripcion:"Juego basado en la película Robocop",
+link:"shrinkme.click/vgD8TR"
+}
