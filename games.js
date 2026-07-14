36f31fa85abf297e572bb599c64ebbbef0411746
Nostalgia Arcade <nostalgiaarcade@outlook.es>
2026-07-14 04:26:01 +0200

    Update games.js

--- games.js
+++ games.js
@@ -1,8 +1,21 @@
+const games = [
+
 {
-nombre:"Robocop 2",
-categoria:"Acción",
-jugadores:"1-2",
-imagen:"https://i.ibb.co/zh8psvqz/Robocop-2.jpg",
-descripcion:"Juego basado en la película Robocop",
-link:"shrinkme.click/vgD8TR"
+    nombre:"Robocop 2",
+    categoria:"Acción",
+    jugadores:"1-2",
+    imagen:"https://i.ibb.co/zh8psvqz/Robocop-2.jpg",
+    descripcion:"Juego basado en la película Robocop",
+    link:"https://shrinkme.click/vgD8TR"
+},
+
+{
+    nombre:"Metal Slug",
+    categoria:"Acción",
+    jugadores:"1-2",
+    imagen:"https://....jpg",
+    descripcion:"Clásico arcade de SNK",
+    link:"https://...."
 }
+
+];
