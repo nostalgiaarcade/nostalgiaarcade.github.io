031c1d0b48206cb337916cac6ee6d96e84300ea0
Nostalgia Arcade <nostalgiaarcade@outlook.es>
2026-07-14 13:07:05 +0200

    Update games.js

--- games.js
+++ games.js
@@ -1,20 +1,29 @@
 const games = [
 
 {
-    nombre:"Pang 3",
-    categoria:"Acción",
-    jugadores:"1-2",
-    imagen:"https://i.ibb.co/M6BqqjN/pang-3.jpg",
-    descripcion:"14/07/2026",
-    link:"shrinkme.click/x2UHp"
+    nombre: "Pang 3",
+    categoria: "Acción",
+    jugadores: "1-2",
+    imagen: "https://i.ibb.co/M6BqqjN/pang-3.jpg",
+
+    fecha: "14/07/2026",
+
+    descripcion: "",
+
+    link: "https://shrinkme.click/x2UHp"
 },
-    {
-    nombre:"Robocop 2",
-    categoria:"Acción",
-    jugadores:"1-2",
-    imagen:"https://i.ibb.co/zh8psvqz/Robocop-2.jpg",
-    descripcion:"14/07/2026",
-    link:"https://shrinkme.click/vgD8TR"
-},
+
+{
+    nombre: "Robocop 2",
+    categoria: "Acción",
+    jugadores: "1-2",
+    imagen: "https://i.ibb.co/zh8psvqz/Robocop-2.jpg",
+
+    fecha: "14/07/2026",
+
+    descripcion: "",
+
+    link: "https://shrinkme.click/vgD8TR"
+}
 
 ];
