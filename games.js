f9e93f465b9fe2ce2fe53efc79c3a2c9768d127b
Nostalgia Arcade <nostalgiaarcade@outlook.es>
2026-07-17 20:32:27 +0200

    Update games.js

--- games.js
+++ games.js
@@ -4,7 +4,7 @@ const games = [
     nombre: "Radikal Bikers",
     categoria: "Acción",
     jugadores: "1",
-    imagen: "https://i.ibb.co/S0vNKqQ/Miniatura-Radikal-Bikers.jpg",
+    imagen: "https://i.ibb.co/4nwMPMKn/Miniatura-Radikal-Bikers.jpg",
 
     fecha: "17/07/2026",
 
