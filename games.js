7846453058929ddadc649e974117aadbd3bb86aa
Nostalgia Arcade <nostalgiaarcade@outlook.es>
2026-07-18 16:02:41 +0200

    Update games.js

--- games.js
+++ games.js
@@ -1,6 +1,19 @@
 const games = [
 
 {
+    nombre: "Neo Turf Masters",
+    categoria: "Deportes",
+    jugadores: "1-2",
+    imagen: "https://i.ibb.co/chr3D8NG/neo-turf-masters.jpg",
+
+    fecha: "18/07/2026",
+
+    descripcion: "",
+
+    link: "https://shrinkme.click/6KXH"
+},     
+    
+{
     nombre: "Golden Axe",
     categoria: "Acción",
     jugadores: "1-2",
