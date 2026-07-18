eb725e452c7a3b778e2a6c10e5ebb60491c57bf8
Nostalgia Arcade <nostalgiaarcade@outlook.es>
2026-07-18 18:10:48 +0200

    Update games.js

--- games.js
+++ games.js
@@ -1,6 +1,19 @@
 const games = [
 
 {
+    nombre: "Bubble Bobble",
+    categoria: "Puzles",
+    jugadores: "1-2",
+    imagen: "https://i.ibb.co/gZM8YfWS/bubble-bobble-1.jpg",
+
+    fecha: "18/07/2026",
+
+    descripcion: "",
+
+    link: "https://shrinkme.click/8Hgvc"
+},         
+    
+{
     nombre: "Puzzle Bobble 4",
     categoria: "Puzles",
     jugadores: "1-2",
