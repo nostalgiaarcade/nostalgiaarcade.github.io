b542a3d4809a369084cabeaa02c7a5898f863ec6
Nostalgia Arcade <nostalgiaarcade@outlook.es>
2026-07-19 13:59:08 +0200

    Update games.js

--- games.js
+++ games.js
@@ -1,6 +1,19 @@
 const games = [
 
 {
+    nombre: "Asterix",
+    categoria: "Acción",
+    jugadores: "1-2",
+    imagen: "https://i.ibb.co/PsgLmn4b/asterix.jpg",
+
+    fecha: "19/07/2026",
+
+    descripcion: "",
+
+    link: "https://shrinkme.click/0vAm"
+},    
+    
+{
     nombre: "Soul Calibur",
     categoria: "Peleas",
     jugadores: "1-2",
