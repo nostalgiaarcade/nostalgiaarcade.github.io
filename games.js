039e717685599b09e6a22842981457820038262c
Nostalgia Arcade <nostalgiaarcade@outlook.es>
2026-07-21 23:54:30 +0200

    Update games.js

--- games.js
+++ games.js
@@ -1,6 +1,19 @@
 const games = [
 
 {
+    nombre: "Puzzle de Pon R",
+    categoria: "Puzles",
+    jugadores: "1-2",
+    imagen: "https://i.ibb.co/99rBVMx4/Puzzle-de-Pon-R.jpg",
+
+    fecha: "22/07/2026",
+
+    descripcion: "",
+
+    link: "https://shrinkme.click/9H3KyW"
+},    
+    
+{
     nombre: "Puzz Loop",
     categoria: "Puzles",
     jugadores: "1-2",
