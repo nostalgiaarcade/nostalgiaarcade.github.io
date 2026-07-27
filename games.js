e7bd08836531d31d12c8ecea50e2165aa6436078
Nostalgia Arcade <nostalgiaarcade@outlook.es>
2026-07-27 14:26:05 +0200

    Update games.js

--- games.js
+++ games.js
@@ -1,6 +1,19 @@
 const games = [
 
 {
+    nombre: "Magical Drop",
+    categoria: "Puzles",
+    jugadores: "1-2",
+    imagen: "https://i.ibb.co/RGySSBpw/magical-drop.jpg",
+
+    fecha: "27/07/2026",
+
+    descripcion: "",
+
+    link: "https://shrinkme.click/yEzpKh"
+},  
+    
+{
     nombre: "Cool Boarders Arcade Jam",
     categoria: "Deportes",
     jugadores: "1",
