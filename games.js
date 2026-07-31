8882a4d6ec5e3b8ed94892c9259b7185d613d8a9
Nostalgia Arcade <nostalgiaarcade@outlook.es>
2026-07-31 15:52:16 +0200

    Update games.js

--- games.js
+++ games.js
@@ -1,6 +1,19 @@
 const games = [
 
 {
+    nombre: "Metal Slug 5",
+    categoria: "Acción",
+    jugadores: "1-2",
+    imagen: "https://i.ibb.co/C50GHmMs/metal-slug-5.jpg",
+
+    fecha: "31/07/2026",
+
+    descripcion: "",
+
+    link: "https://shrinkme.click/XHKz0M"
+}, 
+    
+{
     nombre: "Metal Slug 4",
     categoria: "Acción",
     jugadores: "1-2",
