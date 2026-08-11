872503a7cbc0d71ace3a0f01706b3b4d1d1f51f3
Nostalgia Arcade <nostalgiaarcade@outlook.es>
2026-08-11 14:22:34 +0200

    Update games.js

--- games.js
+++ games.js
@@ -1,6 +1,19 @@
 const games = [
 
 {
+    nombre: "Tecmo World Soccer 96",
+    categoria: "Deportes",
+    jugadores: "1-2",
+    imagen: "https://i.ibb.co/DDzkLyvm/tecmo-world-soccer-96.jpg",
+
+    fecha: "11/08/2026",
+
+    descripcion: "",
+
+    link: "https://shrinkme.click/I7Dp8"
+},
+    
+{
     nombre: "Diet Go Go",
     categoria: "Puzles",
     jugadores: "1-2",
